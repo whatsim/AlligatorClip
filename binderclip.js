8d2b1e7a20a80f2b0e100ed44bf1745a6fbfde72
will ruby <me@willruby.com>
2012-11-16 22:20:40 -0800

    successfully rendering one page. gonna namespace now, should probably read self contained node thing documentation, but will probably wing it.

--- binderclip.js
+++ binderclip.js
@@ -15,28 +15,102 @@
 
 */
 
+var configuration = {
+	baseurl : "/",
+	title : "binderclip",
+	articlesPerPage : 3,
+	trimLength : -1,
+	themeDir : "default"
+}
+
 var fs = require('fs');
 var md = require('node-markdown').Markdown;
 var jade = require('jade');
 var sugar = require('sugar');
 
-var rootPath = require('path').dirname(require.main.filename);
+var util = require('util');
 
-var articles = fs.readdirSync(rootPath + "/articles");
-var pages = fs.readdirSync(rootPath + "/pages");
+var rootPath = require('path').dirname(require.main.filename);
 
 //	note to self, need to warn of parse errors
 //	this will live here for now, but later I think it'd be good to spin this out into a json file.
 
-var configuration = {
-	homepage : "index.jade"
-	title : "binderclip"
-	articlesPerPage : 3,
-	trimLength : -1
+var contentTree = loadContent();
+var navList = createNavList(contentTree);
+
+render(contentTree,navList);
+
+//FUNctions
+
+function loadContent(){
+	var articles = fs.readdirSync(rootPath + "/articles");
+	var pages = fs.readdirSync(rootPath + "/pages");
+	var articlesLength = articles.length;
+	var pagesLength = pages.length;
+
+	var obj = {
+		articles : new Array(),
+		pages : new Array()
+	};
+
+	for(var i = 0; i < articlesLength; i++){
+		var a = contentObjectFromFile(articles[i],'articles')
+		if(a) obj.articles.push(a);
+	}
+
+	for(var i = 0; i < pagesLength; i++){
+		var p = contentObjectFromFile(pages[i],'pages')
+		if(p) obj.pages.push(p);
+	}
+	return obj
 }
 
-var articlesLength = articles.length;
+function contentObjectFromFile(name,section){
+	if(name != '.DS_Store'){
+		var file = fs.readFileSync(rootPath+"/"+section+"/"+name).toString();
+		var time = fs.statSync(rootPath+"/"+section+"/"+name).ctime;
 
-for(var i = 0; i < articlesLength; i++){
-	articles[i]
+		//scuzzy and weak. should do this a different way, maybe an inline json with article metadata, or some other format
+		var title = file.split('\n',1)[0];
+		var body = file.replace(/^.+\n.+\n\n/,'');
+		
+		var obj = {
+			name:name.replace('.md',''),
+			title:title,
+			body: md(body),
+			date: time
+		}
+		return obj
+	}
+}
+
+function createNavList(obj){
+	var nl = new Array();
+	var pagesLength = obj.pages.length;
+	for(var i = 0; i < pagesLength; i++){
+		var n = obj.pages[i].name;
+		var l = n.replace(' ','_');
+		var navItem = {
+			name:n,
+			link:l
+		}
+		nl.push(navItem);
+	}
+	return nl
+}
+
+function render(contentTree,navList){
+	var path = rootPath + "/themes/" + configuration.themeDir + "/views/index.jade";
+	jade.renderFile(path, {p:contentTree.pages[0],nav:navList,pretty:true},function(err,html){
+		if(err){
+			console.error('\nTheres something wrong with the template located at:');
+			console.error(path);
+			console.error('\n'+err + '\n');
+		} else {
+			var oBuf = new Buffer(html);
+			fs.open(rootPath + "/o/" + contentTree.pages[0].name + ".html", 'w',function(err,file){
+				fs.write(file, oBuf, 0, oBuf.length, 0);	
+			});
+		}
+	})
 }
